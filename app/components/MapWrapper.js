90856f4d99030855714f7fe01ea91623f1c51fb8
alan-sj <alansaji272@gmail.com>
2025-06-28 11:05:24 +0530

    Dynamic Loading of Location,update on map 	modified:   app/components/MapWrapper.js 	modified:   app/components/Mapview.js 	new file:   app/components/UserLocation.js 	modified:   package-lock.json 	modified:   package.json

--- app/components/MapWrapper.js
+++ app/components/MapWrapper.js
@@ -1,15 +1,22 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { useState } from "react";
 
 const MapView = dynamic(() => import("./Mapview"), {
   ssr: false,
 });
+const UserLocation=dynamic(() => import("../components/UserLocation"),
+{ ssr: false});
+
 
 const MapWrapper = () => {
+  const[userPosition, setUserPosition] = useState(null);
+
   return (
     <div className="flex h-screen">
-      <MapView />
+      <UserLocation setUserPosition={setUserPosition} />
+      <MapView userPosition={userPosition} />
     </div>
   );
 };
--- app/components/Mapview.js
+++ app/components/Mapview.js
@@ -2,11 +2,21 @@
 
 import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { useEffect, useState  } from "react";
 
-const MapView = () => {
+const MapView = ({userPosition}) => {
+  const defaultPosition=[10.8505, 76.2711];
+  const [position, setPosition]= useState(defaultPosition);
+
+  useEffect(()=>{
+    if(userPosition){
+        setPosition(userPosition);
+    }
+  },[userPosition]);
+
   return (
     <div className="flex-1">
-      <MapContainer center={[10.8505, 76.2711]} zoom={5} style={{ height: "100%", width: "100%" }}>
+      <MapContainer center={position} zoom={5} style={{ height: "100%", width: "100%" }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
